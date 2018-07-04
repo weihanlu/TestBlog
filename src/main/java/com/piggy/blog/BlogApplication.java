c059e800597c113348890fea06adae24c0f4a74f
whl <scissorshand.e@qq.com>
2018-07-04 17:06:11 +0800

    init my blog

---
+++ src/main/java/com/piggy/blog/BlogApplication.java
@@ -0,0 +1,17 @@
+package com.piggy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 启动类
+ *
+ * @author 韦函露
+ * @date 2018/07/04
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+}
